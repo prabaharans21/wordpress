afd80fa3c7284ce4ee588cb407ee1d1e02728bb6
Prabaharan <prabaharansolairaj21@gmail.com>
2023-11-01 16:34:19 +0530

    init

---
+++ wp-content/themes/titan-digital-agency/patterns/banner-block.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Title: Banner Block
+ * Slug: titan-digital-agency/banner-block
+ * Categories: banner
+ * Block Types: core/template-part/banner
+ */
+?>
+
+<!-- wp:cover {"url":"<?php echo esc_url(get_template_directory_uri()) ?>/assets/images/banner-main-image.png","id":7,"dimRatio":0,"overlayColor":"black","minHeight":800,"minHeightUnit":"px","tagName":"main","className":"wp-block-group alignfull"} -->
+<main class="wp-block-cover wp-block-group alignfull" style="min-height:800px"><span aria-hidden="true" class="wp-block-cover__background has-black-background-color has-background-dim-0 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-7" alt="" src="<?php echo esc_url(get_template_directory_uri()) ?>/assets/images/banner-main-image.png" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:columns {"verticalAlignment":"center","align":"wide","textColor":"base","className":"slider-banner"} -->
+<div class="wp-block-columns alignwide are-vertically-aligned-center slider-banner has-base-color has-text-color"><!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:heading {"style":{"typography":{"fontSize":"35px","fontStyle":"normal","fontWeight":"700"}},"textColor":"background"} -->
+<h2 class="wp-block-heading has-background-color has-text-color" style="font-size:35px;font-style:normal;font-weight:700"><?php echo esc_html('Meet Only The Best Consultants Form','titan-digital-agency'); ?><br><?php echo esc_html('All Around The World.','titan-digital-agency'); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"left","textColor":"background","fontSize":"upper-heading"} -->
+<p class="has-text-align-left has-background-color has-text-color has-upper-heading-font-size"><?php echo esc_html('Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley','titan-digital-agency'); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"accent","textColor":"base"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-base-color has-accent-background-color has-text-color has-background wp-element-button" href="#"><strong><?php echo esc_html('Learn More','titan-digital-agency'); ?></strong></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","width":"40%"} -->
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:40%"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div></main>
+<!-- /wp:cover -->
---
+++ wp-content/themes/titan-digital-agency/readme.txt
@@ -0,0 +1,49 @@
+=== Titan Digital Agency ===
+Contributors: titanthemes
+Requires at least: 6.1
+Tested up to: 6.3
+Requires PHP: 5.6
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+Our digital agency theme specializes in crafting extraordinary WordPress experiences for businesses and individuals alike. Our team of skilled developers, designers, and digital strategists is dedicated to bringing your vision to life in the digital realm. With a strong focus on creativity, functionality, and user engagement, we have designed themes that not only look exceptional but also perform seamlessly.Our custom themes are tailored to your unique needs, offering a range of features such as custom fonts, seamless plugin integrations, and user-friendly drag-and-drop editing. Whether you're looking to establish a powerful online presence, optimize for search engines, or revamp your existing website, our comprehensive suite of services is designed to enhance your digital presence, making it easier than ever to connect with your audience and leave a lasting impact.
+
+== Changelog ==
+
+= 1.0 =
+* Released: October 18, 2023
+
+== Copyright ==
+
+Titan Digital Agency WordPress Theme, (C) 2022-2023 Titan Themes
+Titan Digital Agency is distributed under the terms of the GNU GPL.
+
+Titan Digital Agency WordPress Theme incorporates code from Twenty Twenty-Three WordPress Theme, Copyright 2022-2023 WordPress.org
+Twenty Twenty-Three WordPress Theme is distributed under the terms of the GNU GPL
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+License: CC0 1.0 Universal (CC0 1.0)
+License URL: https://pxhere.com/en/license
+Source: https://pxhere.com/en/photo/1450067
+
+Google Web Fonts ( DM Sans, IBM Plex+Sans, Inter, Source Serif) By Google - https://google.com
+
+* DM+Sans, This is the normal family, Licensed under Open Font License, https://fonts.google.com/specimen/DM+Sans?query=DM+Sans
+
+* IBM+Plex+Sans, This is the normal family, Licensed under Open Font License, https://fonts.google.com/specimen/IBM+Plex+Sans?query=IBM+
+
+* Inter, This is the normal family, Licensed under Open Font License, https://fonts.google.com/specimen/Inter?query=inter
+
+* Source+Serif, This is the normal family, Licensed under Open Font License, https://fonts.google.com/specimen/Source+Serif+4?query=Source+Serif
